1531ae6764ad85451adae8dffe477741fe9c326a
Harshitha <harshitha.vangaveti@outlook.com>
2026-04-17 18:23:04 +0000

    Phase 4 Commit 4: Integrated provided head_update into commit flow

--- commit.c
+++ commit.c
@@ -235,6 +235,11 @@ int commit_create(const char *message, ObjectID *commit_id_out) {
         return -1;
     }
 
+    if (head_update(commit_id_out) != 0) {
+        free(data);
+        return -1;
+    }
+
     free(data);
     return 0;
 }
