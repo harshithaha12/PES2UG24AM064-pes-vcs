bc7a3aad91e5bc76bdb2699eb85e81caa38223da
Harshitha <harshitha.vangaveti@outlook.com>
2026-04-17 15:44:01 +0000

    Phase 1 Commit 2: Implemented object header creation logic

--- object.c
+++ object.c
@@ -97,7 +97,30 @@ int object_exists(const ObjectID *id) {
 // Returns 0 on success, -1 on error.
 int object_write(ObjectType type, const void *data, size_t len, ObjectID *id_out) {
     // TODO: Implement
-    (void)type; (void)data; (void)len; (void)id_out;
+    const char *type_str;
+
+    if (type == OBJ_BLOB)
+        type_str = "blob";
+    else if (type == OBJ_TREE)
+        type_str = "tree";
+    else
+        type_str = "commit";
+
+    char header[100];
+    snprintf(header, sizeof(header), "%s %zu", type_str, len);
+
+    size_t header_len = strlen(header) + 1;
+
+    unsigned char *full_data = malloc(header_len + len);
+    if (!full_data)
+        return -1;
+
+    memcpy(full_data, header, header_len);
+    memcpy(full_data + header_len, data, len);
+    compute_hash(full_data, header_len + len, id_out);
+
+    free(full_data);
+    (void)id_out;
     return -1;
 }
 
